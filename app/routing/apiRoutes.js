97f007a1bcbda99b2548a06b952e87aa62ac0616
ajreid <axr490@miami.edu>
2019-12-24 22:55:05 -0500

    added functionality for adding JSON object with survey results to 'friends' array

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,3 +4,9 @@ let friends = require("../data/friends.js");
 server.app.get("/api/friends", function(req, res) {
     res.send(friends);
 });
+
+server.app.post("/api/friends", function(req, res) {
+    friends.push(req.body.surveyResults);
+    console.log(friends);
+    res.json({allFriends: friends});
+})
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,4 +1,5 @@
 let server = require("../../server.js");
+let friends = require("../data/friends.js");
 
 server.app.get("/", function(req, res) {
     res.sendFile(server.path.join(__dirname + "/../public/home.html"));
@@ -6,4 +7,16 @@ server.app.get("/", function(req, res) {
 
 server.app.get("/survey", function(req, res) {
     res.sendFile(server.path.join(__dirname + "/../public/survey.html"));
+})
+
+
+server.app.get("/api/friends", function(req, res) {
+    friends.push(req.body.surveyResults);
+    res.send(friends);
+});
+
+server.app.post("/api/friends", function(req, res) {
+    friends.push(req.body);
+    console.log(friends);
+    res.json(friends);
 })
