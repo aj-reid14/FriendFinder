28e6c0d6724ccd2c4e8f44049077efcc15360e69
ajreid <axr490@miami.edu>
2019-12-24 20:49:44 -0500

    added code for home.html and survey.html, and updated 'htmlRoutes' to load the correct pages accordingly

---
+++ routing/htmlRoutes.js
@@ -0,0 +1,9 @@
+let server = require("../server.js");
+
+server.app.get("/", function(req, res) {
+    res.sendFile(server.path.join(__dirname + "/../public/home.html"));
+})
+
+server.app.get("/survey", function(req, res) {
+    res.sendFile(server.path.join(__dirname + "/../public/survey.html"));
+})
--- server.js
+++ server.js
@@ -1,2 +1,17 @@
 let express = require("express");
 let path = require("path");
+
+let app = express();
+let PORT = 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, function() {
+    console.log("Connected at localhost:" + PORT);
+})
+
+module.exports = {
+    app: app,
+    path: path
+};
